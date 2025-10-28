ca3771e36d991c6fbbbcfd132f0c570776a6dbf7
root <kaden3478@gmail.com>
2025-10-28 13:32:21 +0000

    Added more test and output files

--- src/incollege.cob
+++ src/incollege.cob
@@ -9,11 +9,11 @@
           FILE-CONTROL.
 
               *> Select INPUT-FILE tells COBOL what the input file is
-              SELECT INPUT-FILE ASSIGN TO "test_input/test_input3"
+              SELECT INPUT-FILE ASSIGN TO "test_input/test_input6"
               *> LINE SEQUENTIAL means each line in text is a record
                   ORGANIZATION IS LINE SEQUENTIAL.
               *> OUTPUT-FILE defines what file will have the output stored
-              SELECT OUTPUT-FILE ASSIGN TO "test_output/test_output3.txt"
+              SELECT OUTPUT-FILE ASSIGN TO "test_output/test_output6.txt"
                   ORGANIZATION IS LINE SEQUENTIAL
                   FILE STATUS IS WS-OUTPUT-STATUS.
 
