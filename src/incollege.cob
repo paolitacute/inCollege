d9ba4d7da04b1453e8a85f2c5250fce70c8da229
root <kaden3478@gmail.com>
2025-11-04 14:39:12 +0000

    Added 5 test files based off of the testing requirements in the doc

--- src/incollege.cob
+++ src/incollege.cob
@@ -9,11 +9,11 @@
           FILE-CONTROL.
 
               *> Select INPUT-FILE tells COBOL what the input file is
-              SELECT INPUT-FILE ASSIGN TO "InCollege-Input.txt"
+              SELECT INPUT-FILE ASSIGN TO "test-input/test-input5.txt"
               *> LINE SEQUENTIAL means each line in text is a record
                   ORGANIZATION IS LINE SEQUENTIAL.
               *> OUTPUT-FILE defines what file will have the output stored
-              SELECT OUTPUT-FILE ASSIGN TO "InCollege-Output.txt"
+              SELECT OUTPUT-FILE ASSIGN TO "test-output/test-output5.txt"
                   ORGANIZATION IS LINE SEQUENTIAL
                   FILE STATUS IS WS-OUTPUT-STATUS.
 
